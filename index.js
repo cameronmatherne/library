0d56eef29d586bac597f5268c605184d86665971
Cameron Matherne <cameronpmatherne@gmail.com>
2022-07-25 02:35:08 -0500

    changes

--- index.js
+++ index.js
@@ -24,15 +24,28 @@ const addBtn = document.querySelector('.addBook');
 const container = document.querySelector('.container');
 
 
-
+/*
 addBtn.addEventListener('click', () => {
-    console.log('response');
+});
+*/
+    
+function addCard() {
     const card = document.createElement("div");
     card.className = "card";
     container.appendChild(card);
-});
+
+}
 
 
 
 
 
+
+
+function openForm() {
+    document.getElementById("myForm").style.display = "flex";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
